8400a8ee210bdb086ad519209151c0ef86b011cf
ThomasCarlin <thomasjeffersoncarlin@gmail.com>
2016-02-17 11:38:48 -0500

    mailer stuff

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -1,5 +1,5 @@
 class AuthenticationMailer < ApplicationMailer
-  	default from: "tcarlin11@gmail.com"
+  	default from: "TomCarlinFromVietnam@gmail.com"
 	def authenticate_email(user)
 		@user = user
 		mail(to: "tcarlin11@gmail.com", subject: 'New Case Added')
