876b266c1590b4ee06b86bb7d91aee57387dadf5
ThomasCarlin <thomasjeffersoncarlin@gmail.com>
2016-04-11 15:07:34 -0400

    limit to 10

--- app/models/person.rb
+++ app/models/person.rb
@@ -10,7 +10,7 @@ class Person < ActiveRecord::Base
       :with_created_at_gte
     ]
   )
-  self.per_page = 30
+  self.per_page = 10
 	scope :sorted_by, lambda { |sort_option|
 	  # extract the sort direction from the param value.
 	  direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
